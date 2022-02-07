25a5e89848128316a3c1e488b4cd7c6b3086e82f
Daniel Geremias <daniel.vg19@gmail.com>
2022-02-07 14:48:01 -0300

    Added placeholder state and passed to 'CVPrint' component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,40 @@ import Form from "./Form"
 import CVPrint from "./CVPrint"
 
 export default class Main extends React.Component {
-            
-
     render() {
+        const cv = {
+                name: "Joe",
+                location: "Brazil",
+                email: "joe@gmail.com",
+                phone: "00000-0000",
+                education: [
+                        {
+                            school: "Harvard",
+                            course: "IT",
+                            startDate: "Jan 5th, 2010",
+                            endDate: "Dec 15th, 2014"
+                        },
+                        {
+                            school: "Yale",
+                            course: "IT",
+                            startDate: "Jan 5th, 2010",
+                            endDate: "Dec 15th, 2014"
+                        }
+                    ],
+                experience: [
+                    {
+                        company: "IBM",
+                        position: "Juniot IT",
+                        tasks: "Fix problems and report fixes",
+                        startDate: "Jan 10th, 2015",
+                        endDate: "Dec 18th, 2019"
+                    }
+                    ]
+            }
         return (
             <div className="main">
                 <Form />
-                <CVPrint />
+                <CVPrint cv={cv}/>
             </div>
         )
     }
